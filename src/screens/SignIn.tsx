69636baf91cb2209dad817f1aab1116cf31cea26
hmartins <hmartins224@gmail.com>
2022-07-19 17:08:24 -0300

    feat: developing the login screen and button and input components

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Heading, VStack } from 'native-base';
+import { Heading, Icon, VStack, useTheme } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 export function SignIn() {
+  
+  const { colors } = useTheme();
+  
   return(
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
 
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+
+      <Input 
+        mb={4}
+        placeholder="E-mail" 
+        InputLeftElement={ <Icon ml={4} as={<Envelope color={colors.gray[300]} />} /> }
+      />
+      <Input 
+        mb={8}
+        secureTextEntry
+        placeholder="Senha"
+        InputLeftElement={ <Icon ml={4} as={<Key color={colors.gray[300]} />} /> }
+      />
+
+      <Button title='Entrar' w='full' />
 
     </VStack>
   ); 
