9a029ce35eba3859905306c8abc1243bf0143030
hmartins <hmartins224@gmail.com>
2022-07-20 12:01:28 -0300

    feat: developing the Home screen and yours components

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Heading, Icon, VStack, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -7,7 +7,9 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn() {
-  
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
   
   return(
@@ -22,12 +24,14 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail" 
         InputLeftElement={ <Icon ml={4} as={<Envelope color={colors.gray[300]} />} /> }
+        onChangeText={setName}
       />
       <Input 
         mb={8}
         secureTextEntry
         placeholder="Senha"
         InputLeftElement={ <Icon ml={4} as={<Key color={colors.gray[300]} />} /> }
+        onChangeText={setPassword}
       />
 
       <Button title='Entrar' w='full' />
